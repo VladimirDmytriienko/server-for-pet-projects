c7b9421b3e0d0b549320727a69a459f7fbb7f515
VladimirDmytriienko <89876159+VladimirDmytriienko@users.noreply.github.com>
2025-05-10 00:40:53 +0300

    initial server

---
+++ main.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+require('dotenv').config()
+
+const db = require("./db")
+
+
+const srv = express()
+const PORT = process.env.PORT || 3003
+
+srv.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
+
+srv.use(express.json())
+srv.use(cookieParser())
+srv.use(morgan(":method :url :status"))
+
+srv.listen(PORT, () => console.log(`Server running on port ${PORT}`))
